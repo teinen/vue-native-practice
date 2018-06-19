e47f262c736d236eb680f49aebdfc752bb6d4056
諦念 <teinen.github@gmail.com>
2018-06-20 08:43:08 +0900

    Add Loop component

---
+++ modules/Loop.vue
@@ -0,0 +1,34 @@
+<template>
+  <view class="container">
+    <text class="text-container" v-for="todo in todos" :key="todo.id">
+      {{ todo.text }}
+    </text>
+  </view>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      todos: [
+        { id: 1, text: "Learn JavaScript" },
+        { id: 2, text: "Learn Vue" },
+        { id: 3, text: "Build something awesome" }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-container {
+  color: blue;
+  padding: 2;
+}
+</style>
